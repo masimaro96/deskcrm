65b83e62a7a8f178fd135fe5a2f4122595f7172d
masimaro96 <masimaro1996@gmail.com>
2022-11-22 17:12:39 +0700

    - Session Customer + Assigned Rep. + Attachments + Product => Done - Tab Intruction => In progress - Tab Timeline => In progress

---
+++ deskcrm/src/tabs/component.jsx
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AddIcon from '@mui/icons-material/Add';
+import Avatar from '@mui/material/Avatar';
+import { Stack } from '@mui/system';
+import { Chip, Divider, Grid } from '@mui/material';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Instruction" {...a11yProps(0)} />
+          <Tab label="Timeline" {...a11yProps(1)} />
+          <Tab icon={<AddIcon />} iconPosition="start" label="More" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <Box borderRadius={2} sx={{ flexGrow: 1, p: 2, border: '1px solid grey' }}>
+            <Box display='flex' sx={{ flexGrow: 1}}>
+                <Stack direction="row" spacing={2}>
+                    <Typography noWrap component="div" className=''>
+                        Direction:
+                    </Typography>
+                    <Typography noWrap component="div" className=''>
+                        Outgoing
+                    </Typography>
+                </Stack>
+            </Box>
+            <Box display='flex' sx={{ flexGrow: 1, pt: 1}}>
+                <Stack direction="row" spacing={2}>
+                    <Typography noWrap component="div" className=''>
+                        From:
+                    </Typography>
+                    <Avatar sx={{ width: 30, height: 30}}/>
+                    <Typography noWrap component="div" className=''>
+                        MSM
+                    </Typography>
+                </Stack>
+            </Box>
+            <Box display='flex' sx={{ flexGrow: 1, pt: 1, pb: 3}}>
+                <Stack direction="row" spacing={2}>
+                    <Typography noWrap component="div" className=''>
+                        To:
+                    </Typography>
+                    <Avatar sx={{ width: 30, height: 30}}/>
+                    <Typography noWrap component="div" className=''>
+                        Allan Rey Palban
+                    </Typography>
+                </Stack>
+            </Box>
+            <Box borderRadius={2} sx={{ flexGrow: 1, p: 1 , border: '1px solid black' }}>
+                <Typography noWrap component="div" className=''>
+                    Content
+                </Typography>
+            </Box>
+        </Box>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Divider>
+            <Chip color="secondary" label="2021-03-17" />
+        </Divider>
+        <Grid container spacing={2}>
+            <Grid item xs={2}>
+                
+            </Grid>
+            <Grid item xs={10}>
+                <Box borderRadius={2} sx={{ flexGrow: 1, p: 2, border: '1px solid grey' }} display='flex'>
+                    <Grid item xs={6}>
+                        <Typography noWrap component="div" className=''>
+                            Expected Duration was updated from 1 hour to 1 day by MSR
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography noWrap component="div" className=''>
+                            15:28
+                        </Typography>
+                    </Grid>
+                </Box>
+            </Grid>
+        </Grid>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        
+      </TabPanel>
+    </Box>
+  );
+}
--- deskcrm/src/view-body/component.jsx
+++ deskcrm/src/view-body/component.jsx
@@ -12,6 +12,17 @@ import Avatar from '@mui/material/Avatar';
 import CloseIcon from '@mui/icons-material/Close';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import MobileFriendlyIcon from '@mui/icons-material/MobileFriendly';
+import Stack from '@mui/material/Stack';
+import { blue, green, grey, orange, purple, red } from '@mui/material/colors';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import { IconButton, InputBase } from '@mui/material';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import DescriptionIcon from '@mui/icons-material/Description';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import FolderZipIcon from '@mui/icons-material/FolderZip';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import TabsFax from '../tabs/component';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -70,14 +81,14 @@ export default function ViewBody() {
                 </Grid>
                 <Divider orientation="vertical" flexItem />
                 <Grid item xs={6} sx={{ flexGrow: 1, p: 1 }}>
-                    <Item>xs=6</Item>
+                    <TabsFax />
                 </Grid>
                 <Divider orientation="vertical" flexItem />
-                <Grid item xs sx={{ flexGrow: 1, p: 1 }}>
+                <Grid item xs sx={{ flexGrow: 1, pb: 1 }}>
                     <Box sx={{ flexGrow: 1, p: 2 }}>
-                        <Grid container spacing={2}>
+                        <Grid container>
                             <Grid item xs={6} md={6} display='flex'>
-                                <DragIndicatorIcon />
+                                <DragIndicatorIcon sx={{ color:grey[400], fontSize:20 }} />
                                 <Typography variant="h8" noWrap component="div" className=''>
                                     Customer
                                 </Typography>
@@ -87,20 +98,218 @@ export default function ViewBody() {
                             </Grid>
                         </Grid>
                     </Box>
-                    <Box sx={{ flexGrow: 1, p: 2 }}>
-                        <Grid container spacing={2}>
+                    <Box sx={{ flexGrow: 1, p: 1 }}>
+                        <Grid container sx={{ flexGrow: 1, p: 1 }}>
                             <Grid item xs={6} md={6} display='flex'>
-                                <Avatar />
-                                <Typography variant="h8" noWrap component="div" className=''>
+                                <Avatar sx={{ width: 30, height: 30}}/>
+                                <Typography noWrap component="div" className='name-avatar'>
                                     Allan Rey Palban
                                 </Typography>
                             </Grid>
                             <Grid item xs={6} md={6} className='viewbody-icon-right'>
-                                <MailOutlineIcon />
-                                <MobileFriendlyIcon />
-                                <CloseIcon />
+                                <Stack direction="row" spacing={3}>
+                                    <MailOutlineIcon sx={{ color:green[800], fontSize:20 }} />
+                                    <MobileFriendlyIcon sx={{ color:orange[800], fontSize:20 }} />
+                                    <CloseIcon sx={{ color:red[500], fontSize:20 }}/>
+                                </Stack>
                             </Grid>
                         </Grid>
+                        <Grid container sx={{ flexGrow: 1, p: 1 }}>
+                            <Grid item xs={6} md={6} display='flex'>
+                                <Avatar sx={{ width: 30, height: 30}}/>
+                                <Typography noWrap component="div" className='name-avatar'>
+                                    Allan Rey Palban
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                <Stack direction="row" spacing={3}>
+                                    <MailOutlineIcon sx={{ color:green[800], fontSize:20 }} />
+                                    <MobileFriendlyIcon sx={{ color:orange[800], fontSize:20 }} />
+                                    <CloseIcon sx={{ color:red[500], fontSize:20 }}/>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                        <Paper
+                            component="form"
+                            sx={{ p: '2px 2px', display: 'flex', alignItems: 'center', width: 450 }}>
+                            <InputBase
+                                sx={{ ml: 1, flex: 1 }}
+                                placeholder="Type or click to add an user"
+                                inputProps={{ 'aria-label': 'Type or click to add an user' }}
+                            />
+                            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                            <IconButton sx={{ p: '10px', color:grey[500] }} aria-label="PersonOutline">
+                                <PersonOutlineIcon />
+                            </IconButton>
+                        </Paper>
+                    </Box>
+                    <Divider />
+                    <Box sx={{ flexGrow: 1, p: 2 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6} md={6} display='flex'>
+                                <DragIndicatorIcon sx={{ color:grey[400], fontSize:20 }} />
+                                <Typography variant="h8" noWrap component="div" className=''>
+                                    Assigned Rep.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                <ExpandMoreIcon />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, p: 1 }}>
+                        <Grid container>
+                            <Box borderRadius={5} sx={{ flexGrow: 1, p: 1, background:orange[500] }} display='flex'>
+                                <Grid item xs={6} md={6} display='flex'>
+                                    <Avatar sx={{ width: 30, height: 30}}/>
+                                    <Typography variant="h8" noWrap component="div" className='name-avatar'>
+                                        Allan Rey Palban
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                    <Stack direction="row" spacing={3}>
+                                        <CloseIcon sx={{ color:red[500], fontSize:20 }}/>
+                                    </Stack>
+                                </Grid>
+                            </Box>
+                        </Grid>
+                        <Grid container sx={{ flexGrow: 1, p: 1 }}>
+                            <Grid item xs={6} md={6} display='flex'>
+                                <Avatar sx={{ width: 30, height: 30}}/>
+                                <Typography variant="h8" noWrap component="div" className='name-avatar'>
+                                    MSM
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                <Stack direction="row" spacing={3}>
+                                    <MailOutlineIcon sx={{ color:green[800], fontSize:20 }} />
+                                    <ChatBubbleOutlineIcon sx={{ color:purple[800], fontSize:20 }} />
+                                    <CloseIcon sx={{ color:red[500], fontSize:20 }}/>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                        <Paper
+                            component="form"
+                            sx={{  display: 'flex', alignItems: 'center', width: 450 }}>
+                            <InputBase
+                                sx={{ ml: 1, flex: 1 }}
+                                placeholder="Type or click to add an user"
+                                inputProps={{ 'aria-label': 'Type or click to add an user' }}
+                            />
+                            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                            <IconButton sx={{ p: '10px', color:grey[500] }} aria-label="PersonOutline">
+                                <PersonOutlineIcon />
+                            </IconButton>
+                        </Paper>
+                    </Box>
+                    <Divider />
+                    <Box sx={{ flexGrow: 1, p: 2 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6} md={6} display='flex'>
+                                <DragIndicatorIcon sx={{ color:grey[400], fontSize:20 }} />
+                                <Typography variant="h8" noWrap component="div" className=''>
+                                    Attachments
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                <ExpandMoreIcon />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, p: 1 }}>
+                        <Grid container sx={{ flexGrow: 1, p: 1 }}>
+                            <Box borderRadius={2} sx={{ flexGrow: 1, p: 1, background:grey[100] }} display='flex'>
+                                <Grid item xs={6} md={6} display='flex'>
+                                    <DescriptionIcon sx={{ color:blue[500], fontSize:40 }}/>
+                                    <Box>
+                                    <Typography  noWrap component="div" className='name-doc'>
+                                        sample_list.txt
+                                    </Typography>
+                                    <Typography noWrap component="div" className='time-upload'>
+                                        (18.94 KB) 2019/11/02 12:00:12
+                                    </Typography>
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                    <Stack direction="row" spacing={2}>
+                                        <SaveAltIcon sx={{ color:blue[500], fontSize:20 }} />
+                                        <CloseIcon sx={{ color:red[500], fontSize:20 }}/>
+                                    </Stack>
+                                </Grid>
+                            </Box>
+                        </Grid>
+                        <Grid container sx={{ flexGrow: 1, p: 1 }}>
+                            <Box borderRadius={2} sx={{ flexGrow: 1, p: 1, background:grey[100] }} display='flex'>
+                                <Grid item xs={6} md={6} display='flex'>
+                                    <DescriptionIcon sx={{ color:blue[500], fontSize:40 }}/>
+                                    <Box>
+                                    <Typography  noWrap component="div" className='name-doc'>
+                                        sample_list.txt
+                                    </Typography>
+                                    <Typography noWrap component="div" className='time-upload'>
+                                        (18.94 KB) 2019/11/02 12:00:12
+                                    </Typography>
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                    <Stack direction="row" spacing={2}>
+                                        <SaveAltIcon sx={{ color:blue[500], fontSize:20 }} />
+                                        <CloseIcon sx={{ color:red[500], fontSize:20 }}/>
+                                    </Stack>
+                                </Grid>
+                            </Box>
+                        </Grid>
+                        <Box borderRadius={2} sx={{ flexGrow: 1, p: 1, background:grey[100], border: '1px dashed grey' }} className='drag-drop-file'>
+                            <CloudUploadIcon sx={{ color:grey[500], fontSize:30 }} />
+                            <Typography noWrap component="div" className='upload-file'>
+                                Drag & Drop Files
+                            </Typography>
+                        </Box>
+                        <Box sx={{ flexGrow: 1, p: 1 }} display='flex'>
+                            <FolderZipIcon sx={{ color:blue[500], fontSize:20 }} />
+                            <Typography noWrap component="div" className='download-file'>
+                                Download all
+                            </Typography>
+                        </Box>
+                        <Divider />
+                        <Box sx={{ flexGrow: 1, p: 1 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6} md={6} display='flex'>
+                                    <DragIndicatorIcon sx={{ color:grey[400], fontSize:20 }} />
+                                    <Typography variant="h8" noWrap component="div" className=''>
+                                        Product
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                    <ExpandMoreIcon />
+                                </Grid>
+                            </Grid>
+                            <Grid container sx={{ flexGrow: 1, p: 1 }}>
+                                <Grid item xs={6} md={6} display='flex'>
+                                    <Typography noWrap component="div" className='name-product'>
+                                        Woman Peacoat
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={6} md={6} className='viewbody-icon-right'>
+                                    <Stack direction="row" spacing={3}>
+                                        <CloseIcon sx={{ color:red[500], fontSize:20 }}/>
+                                    </Stack>
+                                </Grid>
+                            </Grid>
+                            <Paper
+                                component="form"
+                                sx={{  display: 'flex', alignItems: 'center', width: 450 }}>
+                                <InputBase
+                                    sx={{ ml: 1, flex: 1 }}
+                                    placeholder="Type or click to add a product"
+                                    inputProps={{ 'aria-label': 'Type or click to add a product' }}
+                                />
+                                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                                <IconButton sx={{ p: '10px', color:grey[500] }} aria-label="PersonOutline">
+                                    <InventoryIcon />
+                                </IconButton>
+                            </Paper>
+                        </Box>
                     </Box>
                 </Grid>
             </Grid>
